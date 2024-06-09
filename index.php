223fcb9dd4c9f34911077f0fa0839a417ce5b75a
SesaiPK <sebastian.grudnik@student.pk.edu.pl>
2024-06-09 18:52:39 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+require 'Routing.php';
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+Router::get('', 'DefaultController');
+Router::get('login','DefaultController');
+Router::get('projects', 'DefaultController');
+Router::run($path);
---
+++ public/views/login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="../public/css/style.css" />
+    <title>Wiedmolol login</title>
+    </title>
+</head>
+
+
+<body>
+
+    <div class="container">
+
+        <form>
+            <div class="login-buttons">
+                <button name="withFacebook"><img src="../public/img/fb.svg"><span>Continue with Facebook</span></button>
+                <button name="withGoogle"><img src="../public/img/google.svg"><span>Continue with Google</span></button>
+                <button name="withApple"><img src="../public/img/apple.svg"><span>Continue with Apple</span></button>
+            </div>
+            <input name="email" type="text" placeholder="email@email.com">
+            <input name="password" type="password" placeholder="password">
+            <button name="logIn" type="submit">Log in</button>
+        </form>
+    </div>
+    <div class="signup-bar">
+        <p>Don't have an account?</p>
+        <button name="signUp">Sign up</button>
+    </div>
+</body>
+
+</html>
